cafa043dc34eb13e9706562fb72971a96824c984
908729783 <133336383+908729783@users.noreply.github.com>
2023-05-26 23:00:58 +0800

    Config.java

--- src/main/java/cn/jja8/newbinggogo/Config.java
+++ src/main/java/cn/jja8/newbinggogo/Config.java
@@ -4,5 +4,5 @@ import cn.jja8.config.tool.Conf;
 
 public class Config {
     @Conf static boolean joinStats = true;
-    @Conf static String[] cookies = {};
+    @Conf static String[] cookies = {_U=13Po1iKnL1mty_-548RUgC2NYiNQvPN9HhnkJY24--8Ca-_Ei8TgWnOag5RRpW3z35oGTAN_ykb1h75op5vFn6OTNPzWkzZyh7als3g8Yrkb86ImactkODlepHMJnPZdzw_qZZ4eZLst2CxejkLdbNonBPyA7_lbRFVfTnXuEdA3PPWsEKEO7jggPOfFOPfmEiIj2pQ2EMxAEjcOLeVeczA};
 }
